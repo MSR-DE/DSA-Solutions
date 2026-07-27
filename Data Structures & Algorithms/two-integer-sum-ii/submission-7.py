435fbdacc8f5541fd5283821ca59989c299fae69
Mohit Singh Rathore <123836601+MSR-DE@users.noreply.github.com>
2026-07-28 01:31:06 +0530

    Add: two-integer-sum-ii - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        ''' target is bigger or smaller than the result 
+            would define if left moves or right'''
+        l=0
+        r=len(numbers)-1
+
+        while l<r:
+            if l<r and numbers[l]+numbers[r]<target:
+                l+=1
+            elif l<r and numbers[l]+numbers[r]>target:
+                r-=1
+            elif l<r and numbers[l]+numbers[r]==target:
+                return[l+1, r+1]
+
+
+        
